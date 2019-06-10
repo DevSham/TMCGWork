3c22fc5830a395ea20d9b3c6e9aa2a9cf77bae1a
shamim2019 <kjingo12@gmail.com>
2019-06-10 14:52:47 -0400

    add indext route and delete a method from controller

--- routes/web.php
+++ routes/web.php
@@ -10,26 +10,5 @@
 |
 */
 
-Route::get('/', function () {
-     return view('postsPages.create');
- });
-// //passing variables to a routes.
-// Route::get('/numbers/{age}/{name}', function ($age, $name) {
-//     $sex ="female";
-//     return "her age is".$age."and her name is".$name."\n she is a".$sex;
-// });
-// //naming routes
-// Route::get('adminn/sham/home', array('as'=>'adminn.sham', function (){
-//     $url = route('adminn.sham');
-//     return "this is the". $url;
-// }));
-//Route::get('/post/{id}','Product1@index');
-//creating routes for all methods in product1 at once.
-//Route::resource('post','Product1');
-// Route::get('/home/{name}/{id}/{sex}', 'Product1@Home');
-// Route::get('/contact', 'Product1@contact');
-// Route::get('/home/{name}/{id}/{sex}', 'Product1@Home');
-// Route::get('/users', 'Shamim@usee');
-// Route::get('/read', 'Product1@readData');
-// Route::get('/update', 'Product1@getspecifc');
+Route::get('/', 'PostsController@index');
 Route::resource('/posts', 'PostsController');
